79b69770565056a2bd4cffe71a5c4902981d8de1
Alan Vasconcelos <alan@MacBook-Pro-de-Alan-2.local>
2020-09-02 11:17:00 -0300

    Adding transition for theme color swap

--- components/layout/index.js
+++ components/layout/index.js
@@ -5,8 +5,9 @@ import { gray } from "../Colors";
 // import { Container } from './styles';
 
 const Wrapper = styled.div`
+    transition: 0.4s;
     background: ${({ theme }) =>
-        theme.dark ? gray[900] : theme.global.backgroundColor}; ;
+        theme.dark ? gray[900] : theme.global.primaryColor[100]}; ;
 `;
 
 export default Wrapper;
