8123d25aa062fa21c4a55916b242dac415cf3c1d
Alan Vasconcelos <alan@MacBook-Pro-de-Alan-2.local>
2020-09-02 01:33:44 -0300

    Removing ThemeColor in class constructor

--- components/Theme.jsx
+++ components/Theme.jsx
@@ -2,26 +2,19 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { blue, red, gray, purple } from "./Colors";
 class ThemeParams {
-    constructor(
-        primaryColor,
-        secondaryColor,
-        regularColor,
-        cornerRadius,
-        themeColor
-    ) {
+    constructor(primaryColor, secondaryColor, regularColor, cornerRadius) {
         this.primaryColor = primaryColor;
         this.backgroundColor = primaryColor[100];
         this.secondaryColor = secondaryColor;
         this.regularColor = regularColor;
         this.cornerRadius = cornerRadius;
-        this.themeColor = themeColor || "light";
     }
 }
 
 const Theme = ({ children, dark }) => {
     console.log(dark);
     const myTheme = {
-        global: new ThemeParams(purple, gray[100], "#fff", 6),
+        global: new ThemeParams(purple, gray[100], "#fff", 4),
         dark,
     };
 
