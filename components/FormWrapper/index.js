1c72d5f0baa943c361845827425ab0eee4d4c42e
Alan Vasconcelos <alan@MacBook-Pro-de-Alan-2.local>
2020-09-02 13:26:50 -0300

    Elevation Option

--- components/FormWrapper/index.js
+++ components/FormWrapper/index.js
@@ -5,7 +5,7 @@ import { gray } from "../Colors";
 const FormWrapper = styled.div`
     background: ${({ theme }) => (theme.dark ? gray[800] : `#fff`)};
     width: 450px;
-    box-shadow: 0 3px 20px #00000010;
+    box-shadow: ${({ theme }) => theme.global.elevation};
     padding: 2.3rem 1.8rem;
     border-radius: ${({ theme }) =>
         theme.global.cornerRadius > 15 ? 15 : theme.global.cornerRadius}px;
--- components/Theme.jsx
+++ components/Theme.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { blue, red, purple, orange } from "./Colors";
+
+const themeOptions = {
+    primaryColor: purple,
+    danger: red,
+    cornerRadius: 6,
+    elevation: 3,
+};
+
 class ThemeParams {
-    constructor(primaryColor, danger, cornerRadius) {
+    constructor({ primaryColor, danger, cornerRadius, elevation }) {
+        const depth = {
+            1: "0 1px 1px 0 rgba(0, 0, 0, 0.16)",
+            2: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.2)",
+            3: "0 3px 6px 0 rgba(0, 0, 0, 0.16), 0 3px 6px 0 rgba(0, 0, 0, 0.23)",
+            4: "0 10px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 6px 0 rgba(0, 0, 0, 0.26)",
+            5: "0 14px 28px 0 rgba(0, 0, 0, 0.25), 0 10px 10px 0 rgba(0, 0, 0, 0.26)",
+        };
         this.primaryColor = primaryColor;
         this.danger = danger;
         this.cornerRadius = cornerRadius;
+        this.elevation = depth[elevation];
     }
 }
 
 const Theme = ({ children, dark }) => {
     const myTheme = {
-        global: {
-            primaryColor: purple,
-            danger: red,
-            cornerRadius: 6,
-        },
+        global: new ThemeParams(themeOptions),
         dark,
     };
 
