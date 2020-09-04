72dcebf9c105f0fcfbfcb6f85a0b2c9a9ebd5f4f
Alan Vasconcelos <alan@MacBook-Pro-de-Alan-2.local>
2020-09-04 14:52:43 -0300

    Heading variants

--- components/Heading/Index.js
+++ components/Heading/Index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
-const Heading = styled.h1`
-    color: ${({ theme }) =>
-        theme.dark
-            ? theme.global.primaryColor[400]
-            : theme.global.primaryColor.normal};
+function getColor({ color, theme }) {
+    if (color) {
+        return color;
+    }
+
+    if (theme.dark) {
+        return theme.global.primaryColor[400];
+    } else {
+        return theme.global.primaryColor.normal;
+    }
+}
+
+const HeadingOne = styled.h1`
+    color: ${(props) => getColor(props)};
     position: relative;
     display: flex;
     justify-content: center;
@@ -20,5 +29,32 @@ const Heading = styled.h1`
         top: 0;
     }
 `;
+const HeadingTwo = styled.h2`
+    color: ${({ theme }) =>
+        theme.dark
+            ? theme.global.primaryColor[400]
+            : theme.global.primaryColor.normal};
+    display: flex;
+    justify-content: center;
+    padding-top: 1.1rem;
+`;
+const HeadingThree = styled.h3`
+    color: ${(props) => props.color};
+`;
+const HeadingFour = styled.h4`
+    color: ${(props) => props.color};
+`;
+
+const Heading = (props) => {
+    if (props.level === 1) {
+        return <HeadingOne {...props}>{props.children}</HeadingOne>;
+    } else if (props.level === 2) {
+        return <HeadingTwo {...props}>{props.children}</HeadingTwo>;
+    } else if (props.level === 3) {
+        return <HeadingThree {...props}>{props.children}</HeadingThree>;
+    } else if (props.level === 4) {
+        return <HeadingFour {...props}>{props.children}</HeadingFour>;
+    }
+};
 
 export default Heading;
