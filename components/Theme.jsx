240ae9721b6d5989b4181ce63c9174ad54e8b09b
Alan Vasconcelos <alan@MacBook-Pro-de-Alan-2.local>
2020-10-02 01:11:34 -0300

    Refactoring theme class

--- components/Theme.jsx
+++ components/Theme.jsx
@@ -2,32 +2,38 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { blue, red, purple, orange } from "./Colors";
 
+const isDynamic = false;
+
 /**
  * Setting Up Theme Object
  */
 const themeObject = {
     global: {
-        primaryColor: purple,
+        primaryColor: blue,
         cornerRadius: 4,
-        elevation: 5,
+        elevation: getElevation(2),
         danger: red,
     },
     dark: false,
 };
 
+function getElevation(amount) {
+    const depth = {
+        1: "0 1px 1px 0 rgba(0, 0, 0, 0.16)",
+        2: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.2)",
+        3: "0 3px 6px 0 rgba(0, 0, 0, 0.16), 0 3px 6px 0 rgba(0, 0, 0, 0.23)",
+        4: "0 10px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 6px 0 rgba(0, 0, 0, 0.26)",
+        5: "0 14px 28px 0 rgba(0, 0, 0, 0.25), 0 10px 10px 0 rgba(0, 0, 0, 0.26)",
+    };
+    return depth[amount];
+}
+
 class ThemeParams {
     constructor({ primaryColor, danger, cornerRadius, elevation }) {
-        const depth = {
-            1: "0 1px 1px 0 rgba(0, 0, 0, 0.16)",
-            2: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.2)",
-            3: "0 3px 6px 0 rgba(0, 0, 0, 0.16), 0 3px 6px 0 rgba(0, 0, 0, 0.23)",
-            4: "0 10px 20px 0 rgba(0, 0, 0, 0.19), 0 6px 6px 0 rgba(0, 0, 0, 0.26)",
-            5: "0 14px 28px 0 rgba(0, 0, 0, 0.25), 0 10px 10px 0 rgba(0, 0, 0, 0.26)",
-        };
         this.primaryColor = primaryColor;
         this.danger = danger;
         this.cornerRadius = cornerRadius;
-        this.elevation = depth[elevation];
+        this.elevation = getElevation(elevation);
     }
 }
 
@@ -53,6 +59,8 @@ const Theme = ({ children, dark, primaryColor, radius, elevation }) => {
         dark,
     };
 
-    return <ThemeProvider theme={dynamicTheme}>{children}</ThemeProvider>;
+    const selectedTheme = isDynamic ? dynamicTheme : themeObject;
+
+    return <ThemeProvider theme={selectedTheme}>{children}</ThemeProvider>;
 };
 export default Theme;
